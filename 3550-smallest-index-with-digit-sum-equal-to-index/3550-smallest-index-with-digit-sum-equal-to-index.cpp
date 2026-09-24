95ebb204810eb4a61f1b7f3611a53b0d4f0d87cf
Naivyam Pandey <naivyampandey7515@gmail.com>
2026-09-24 23:16:03 +0530

    Time: 0 ms (100%), Space: 30.8 MB (98.04%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        
+        for(int i = 0; i < nums.size(); i++){
+
+            int temp = 0;
+            while(nums[i] != 0){
+                temp += nums[i]%10;
+                nums[i] /= 10;
+            }
+
+            if(temp == i) return i;
+
+        }
+
+        return -1;
+    }
+};
