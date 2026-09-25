5a07beb31e0e55368a4b630e62933a0b97394adb
Naivyam Pandey <naivyampandey7515@gmail.com>
2026-09-25 11:49:08 +0530

    Time: 0 ms (100%), Space: 10 MB (52.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int i = 0;
+        int j = s.length();
+
+        while(i < j){
+
+            if(!((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || isdigit(s[i]))){
+                i++;
+                continue;
+            }
+            if(!((s[j] >= 'A' && s[j] <= 'Z') || (s[j] >= 'a' && s[j] <= 'z') || isdigit(s[j]))){
+                j--;
+                continue;
+            }
+            s[i] = tolower(s[i]);
+            s[j] = tolower(s[j]);
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+};
