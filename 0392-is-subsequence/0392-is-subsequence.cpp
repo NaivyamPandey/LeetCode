c08c5106e01b226e64a45d25880d3df742109035
Naivyam Pandey <naivyampandey7515@gmail.com>
2026-09-25 13:26:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (31.55%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        if(s.length() > t.length()) return false;
+
+        int i = 0, j = 0;
+
+        while(j < t.length()){
+
+            if(s[i] == t[j]){
+                i++;
+            } 
+            j++;
+        }
+
+        if(i == s.length()) return true;
+
+        return false;
+
+    }
+};
